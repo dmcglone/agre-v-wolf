b9c1de36e04c3cc08ad9307fd2308d38b6a1ee5c
Daniel McGlone <dmcglone@azavea.com>
2017-10-23 23:25:28 -0400

    updated waffle title

--- charts.R
+++ charts.R
@@ -8,7 +8,7 @@ desired_order <- c("2", "1", "14", "13", "17", "8", "15", "12", "6", "18", "7", 
 cds_m$CD115FP <- factor( as.character(cds_m$CD115FP), levels=desired_order )
 
 ggplot(cds_m,aes(x = CD115FP,y = value)) + 
-  geom_bar(aes(fill = variable),stat = "identity",position = "dodge") +
+  geom_bar(aes(fill = variable),stat = "identity", position = "dodge", width=0.7 ) +
   scale_fill_manual(values = c("DEM_PERC" = "#3EC0F3", "GOP_PERC" = "#F37561"), labels=c("Democratic %", "Republican %")) +
   labs(x = "Congressional District", y="Normal Percentage of Vote", title = "Pennsylvania Congressional Districts", subtitle = "Ranked by normal Democratic vote percentage") +
   guides(fill=guide_legend(title=NULL))
--- waffle-charts.R
+++ waffle-charts.R
@@ -1,75 +1,75 @@
 library(waffle)
 
 iron(
-  waffle(title="District 1", c("GOP"=22, "Democratic"=78), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="1st Congressional District", c("GOP"=22, "Democratic"=78), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 iron(
-  waffle(title="District 2", c("GOP"=11, "Democratic"=89), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="2nd Congressional District", c("GOP"=11, "Democratic"=89), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 iron(
-  waffle(title="District 3", c("GOP"=53, "Democratic"=47), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="3rd Congressional District", c("GOP"=53, "Democratic"=47), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 iron(
-  waffle(title="District 4", c("GOP"=59, "Democratic"=41), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="4th Congressional District", c("GOP"=59, "Democratic"=41), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 iron(
-  waffle(title="District 5", c("GOP"=54, "Democratic"=46), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="5th Congressional District", c("GOP"=54, "Democratic"=46), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 iron(
-  waffle(title="District 6", c("GOP"=52, "Democratic"=48), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="6th Congressional District", c("GOP"=52, "Democratic"=48), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 iron(
-  waffle(title="District 7", c("GOP"=52, "Democratic"=48), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="7th Congressional District", c("GOP"=52, "Democratic"=48), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 iron(
-  waffle(title="District 8", c("GOP"=49, "Democratic"=51), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="8th Congressional District", c("GOP"=49, "Democratic"=51), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 iron(
-  waffle(title="District 9", c("GOP"=56, "Democratic"=44), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="9th Congressional District", c("GOP"=56, "Democratic"=44), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 iron(
-  waffle(title="District 10", c("GOP"=60, "Democratic"=40), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="10th Congressional District", c("GOP"=60, "Democratic"=40), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 iron(
-  waffle(title="District 11", c("GOP"=54, "Democratic"=46), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="11th Congressional District", c("GOP"=54, "Democratic"=46), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 iron(
-  waffle(title="District 12", c("GOP"=51, "Democratic"=49), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="12th Congressional District", c("GOP"=51, "Democratic"=49), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 iron(
-  waffle(title="District 13", c("GOP"=35, "Democratic"=65), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="13th Congressional District", c("GOP"=35, "Democratic"=65), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 iron(
-  waffle(title="District 14", c("GOP"=30, "Democratic"=70), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="14th Congressional District", c("GOP"=30, "Democratic"=70), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 iron(
-  waffle(title="District 15", c("GOP"=51, "Democratic"=49), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="15th Congressional District", c("GOP"=51, "Democratic"=49), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 iron(
-  waffle(title="District 16", c("GOP"=56, "Democratic"=44), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="16th Congressional District", c("GOP"=56, "Democratic"=44), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 iron(
-  waffle(title="District 17", c("GOP"=57, "Democratic"=43), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="17th Congressional District", c("GOP"=57, "Democratic"=43), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 iron(
-  waffle(title="District 18", c("GOP"=52, "Democratic"=48), colors=c("#F37561", "#3EC0F3"), rows=5)
+  waffle(title="18th Congressional District", c("GOP"=52, "Democratic"=48), colors=c("#F37561", "#3EC0F3"), rows=5)
 )
 
 
